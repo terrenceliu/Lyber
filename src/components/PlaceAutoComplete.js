2b102305fa70bfc5a8377f3fb5bd4a03cd9c8200
Terrence <terrenceliu@rice.edu>
2018-06-10 00:37:30 -0500

    add departure & destination select and display

--- src/components/PlaceAutoComplete.js
+++ src/components/PlaceAutoComplete.js
@@ -7,61 +7,171 @@ class PlaceAutoComplete extends Component {
     constructor() {
         super();
         this.state = {
-            autocomplete: undefined,
-            addr: undefined
+            posMarker: undefined,
+            deparAC: undefined,
+            deparPlace: undefined,
+            destAC: undefined,
+            destPlace: undefined
         }
     }
 
     loadAutoComplete() {
         if (this.props && this.props.google) {
-            
-            console.log("Has google!");
-
+            // Find props
             const { google } = this.props;
             const maps = google.maps;
 
-            const inputRef = this.refs.acinput;
-            const node = ReactDOM.findDOMNode(inputRef);
-            
-            const autocomplete = new maps.places.Autocomplete(node)
-            
-            autocomplete.addListener('place_changed', function() {
-                var place = autocomplete.getPlace();
+            // Find ref hooks
+            const deparRef = this.refs.deparRef;
+            const destRef = this.refs.destRef;
+            const mapRef = this.refs.mapRef;
 
-                console.log(place);
-                
+            // Find hook nodes
+            const deparNode = ReactDOM.findDOMNode(deparRef);
+            const destNode = ReactDOM.findDOMNode(destRef);
+            const mapNode = ReactDOM.findDOMNode(mapRef);
+
+            // Instantiate Autocomplete to the nodes
+            var deparAC = new maps.places.Autocomplete(deparNode);
+            var destAC = new maps.places.Autocomplete(destNode);
+            var map = new maps.Map(mapNode, {
+                // Set center to Jones College lol
+                center: {
+                    lat: 29.721482, 
+                    lng: -95.396827
+                },
+                zoom: 13
+            });
+            var posMarker = new maps.Marker({
+                map: map,
+                anchorPoint: new maps.Point(0, 0)
+            });
+            
+
+            deparAC.addListener('place_changed', function() {
+                var place = deparAC.getPlace();
+
+                posMarker.setVisible(false);
+
+                if (!place.geometry) {
+                    alert("Details unavailable for input: " + place.name + ".");
+                    return;
+                }
+
+                if (place.geometry.viewport) {
+                    map.fitBounds(place.geometry.viewport);
+                } else {
+                    map.setCenter(place.geometry.location);
+                    map.setZoom(16);
+                }
+
+                posMarker.setPosition(place.geometry.location);
+                posMarker.setVisible(true);
+
                 this.setState({
-                    addr: place.formatted_address
+                    deparPlace: place
                 })
 
-                console.log(this.state);
+                console.log(place);
+
             }.bind(this));
 
-            this.setState({
-                autocomplete: autocomplete
-            });
+            destAC.addListener('place_changed', function() {
+                var place = destAC.getPlace();
+
+                posMarker.setVisible(false);
+
+                if (!place.geometry) {
+                    alert("Details unavailable for input: " + place.name + ".");
+                    return;
+                }
+
+                if (place.geometry.viewport) {
+                    map.fitBounds(place.geometry.viewport);
+                } else {
+                    map.setCenter(place.geometry.location);
+                    map.setZoom(16);
+                }
+
+                posMarker.setPosition(place.geometry.location);
+                posMarker.setVisible(true);
+
+                this.setState({
+                    destPlace: place
+                })
+
+                console.log(place);
+            }.bind(this));
 
 
         }
+
+
+        // if (this.props && this.props.google) {
+            
+        //     console.log("Has google!");
+
+        //     const { google } = this.props;
+        //     const maps = google.maps;
+            
+        //     const inputRef = this.refs.acinput;
+        //     const node = ReactDOM.findDOMNode(inputRef);
+            
+        //     const autocomplete = new maps.places.Autocomplete(node)
+            
+        //     autocomplete.addListener('place_changed', function() {
+        //         var place = autocomplete.getPlace();
+                
+        //         console.log(place);
+                
+        //         this.setState({
+        //             addr: place.formatted_address
+        //         })
+                
+        //         console.log(this.state);
+        //     }.bind(this));
+
+        //     this.setState({
+        //         autocomplete: autocomplete
+        //     });
+        // }
     }
-    
+
     componentDidMount() {
         this.loadAutoComplete();
     }
 
-    componentDidUpdate() {
-        console.log("Update!");
-        console.log(this.state.location);
-    }
-
     render() {
+        const style = {
+            width: '50vw',
+            height: '50vh',
+            position: relative
+        }
+
         return (
             <div>
-                <h3> Search Places </h3>
-                <input ref="acinput" />
+                
+                <input ref="deparRef" />
+                <input ref="destRef" />
+
                 <div>
-                    <p> Location: {this.state.addr} </p>
+                {    
+                    this.state.deparPlace && 
+                    <p>
+                        [Test] Departure Address: { this.state.deparPlace.formatted_address } 
+                    </p>
+                }
                 </div>
+                <div>
+                {
+                    this.state.destPlace && 
+                    <p>
+                        [Test] Destination Address: { this.state.destPlace.formatted_address }
+                    </p>
+                }    
+                </div>
+                
+                <div ref="mapRef" style={style}> Map Place Holder </div>
             </div>
         )
     }
